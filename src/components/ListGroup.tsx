a77efbd3b7110e87e7e4a660a6659cde68373574
Karla Glatt <58083412+kglatt@users.noreply.github.com>
2024-01-13 21:37:40 -0500

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,21 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;;
+}
+  return (
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
